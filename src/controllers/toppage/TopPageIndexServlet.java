a5d8a8b30abd783d7eb84f1adae35cd070e86167
kazuki.sato <toaru1018@yahoo.co.jp>
2020-08-02 09:54:37 +0900

    Modify top page

--- src/controllers/toppage/TopPageIndexServlet.java
+++ src/controllers/toppage/TopPageIndexServlet.java
@@ -1,13 +1,19 @@
 package controllers.toppage;
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.persistence.EntityManager;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.Task;
+import models.User;
+import utils.DBUtil;
 
 /**
  * Servlet implementation class TopPageIndexServlet
@@ -29,7 +35,7 @@ public class TopPageIndexServlet extends HttpServlet {
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // TODO Auto-generated method stub
-       /* EntityManager em = DBUtil.createEntityManager();
+        EntityManager em = DBUtil.createEntityManager();
 
         User login_user = (User)request.getSession().getAttribute("login_user");
 
@@ -45,20 +51,20 @@ public class TopPageIndexServlet extends HttpServlet {
                                   .setMaxResults(15)
                                   .getResultList();
 
-        List<Routine> routines = em.createNamedQuery("getMyAllRoutines", Routine.class)
+        /*List<Routine> routines = em.createNamedQuery("getMyAllRoutines", Routine.class)
                 .setParameter("user", login_user)
                 .setFirstResult(15 * (page - 1))
                 .setMaxResults(15)
-                .getResultList();
+                .getResultList();*/
 
         long tasks_count = (long)em.createNamedQuery("getMyTasksCount", Long.class)
                                      .setParameter("user", login_user)
                                      .getSingleResult();
 
 
-        long routines_count = (long)em.createNamedQuery("getMyRoutinesCount", Long.class)
+        /*long routines_count = (long)em.createNamedQuery("getMyRoutinesCount", Long.class)
                                      .setParameter("user", login_user)
-                                     .getSingleResult();
+                                     .getSingleResult();*/
 
         em.close();
 
@@ -66,7 +72,7 @@ public class TopPageIndexServlet extends HttpServlet {
         request.setAttribute("tasks_count", tasks_count);
         request.setAttribute("page", page);
 
-        request.setAttribute("routines", routines);
+        /*request.setAttribute("routines", routines);
         request.setAttribute("routines_count", routines_count);
         request.setAttribute("page", page);*/
 
